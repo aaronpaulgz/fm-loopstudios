65f0db2541c323e043d2bb10f5687fb17ca3fd44
Aaron Gonzalez <aarong.1503@gmail.com>
2021-03-18 08:39:45 -0500

    Project init

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let $hamburger = document.getElementById("hamburger");
+let $close = document.getElementById("close");
+let $mob_menu = document.getElementById("mob_menu");
+
+const showMenu = () => {
+  $mob_menu.style.display = "block";
+};
+
+const hideMenu = () => {
+  $mob_menu.style.display = "none";
+};
